4bf9a0bbd2591efd66c4ee31b56b36aee28086ae
Muhammad Ibraheem <ibraheem.mql@gmail.com>
2021-10-31 21:25:32 +0500

    first commit

---
+++ src/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => (
+  <div>
+    <h2>About Page</h2>
+    <p>This is mostly about us.</p>
+    <p>You can make it about yourself.</p>
+  </div>
+)
---
+++ src/Contact.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Contact = () => (
+  <div>
+    <h2>Contact Us</h2>
+    <p>You Can add form your fill details</p>
+  </div>
+)
